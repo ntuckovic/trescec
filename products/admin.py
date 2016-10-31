168f47fc39bf2a00d9a4677b67eaf8705867377c
ntuckovic <nikola.tuckovic@styria.hr>
2016-10-31 18:30:28 +0100

    inicijalni setting za ck

--- products/admin.py
+++ products/admin.py
@@ -10,12 +10,38 @@ from suit_ckeditor.widgets import CKEditorWidget
 
 from .models import Product
 
+toolbarGroups = [
+		{ 'name': 'document', 'groups': [ 'mode', 'document', 'doctools' ] },
+		{ 'name': 'clipboard', 'groups': [ 'clipboard', 'undo' ] },
+		{ 'name': 'editing', 'groups': [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ 'name': 'forms', 'groups': [ 'forms' ] },
+		{ 'name': 'basicstyles', 'groups': [ 'basicstyles', 'cleanup' ] },
+		{ 'name': 'paragraph', 'groups': [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ 'name': 'links', 'groups': [ 'links' ] },
+		{ 'name': 'insert', 'groups': [ 'insert' ] },
+		{ 'name': 'styles', 'groups': [ 'styles' ] },
+		{ 'name': 'colors', 'groups': [ 'colors' ] },
+		{ 'name': 'tools', 'groups': [ 'tools' ] },
+		{ 'name': 'others', 'groups': [ 'others' ] },
+		{ 'name': 'about', 'groups': [ 'about' ] }
+	];
+
+removeButtons = 'Flash,ShowBlocks,About,Scayt,TextField,Radio,Checkbox,Form,Textarea,Button,Select,HiddenField,ImageButton,SelectAll,Find,Replace,Print,Preview,NewPage,Save,Templates,CreateDiv,Language,BidiRtl,BidiLtr';
+
 
 class PageForm(ModelForm):
     class Meta:
         widgets = {
-            'short_description': CKEditorWidget(editor_options={'startupFocus': True}),
-            'long_description': CKEditorWidget(editor_options={'startupFocus': True})
+            'short_description': CKEditorWidget(editor_options={
+                'startupFocus': True,
+                'toolbarGroups': toolbarGroups,
+                'removeButtons': removeButtons
+            }),
+            'long_description': CKEditorWidget(editor_options={
+                'startupFocus': True,
+                'toolbarGroups': toolbarGroups,
+                'removeButtons': removeButtons
+            })
         }
 
 @admin.register(Product)
