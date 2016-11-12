04528730666c2975e45ec9c2069f9b34be938475
ntuckovic <nikola.tuckovic@styria.hr>
2016-11-12 15:56:01 +0100

    hekling form

--- trescec/static/dev/app/index.jsx
+++ trescec/static/dev/app/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
+import serialize from 'form-serialize';
+import hasClass from 'react-kit/hasClass';
 
 import '../sass/custom.scss';
 
@@ -8,10 +10,30 @@ class App extends React.Component {
     constructor(props) {
       super(props);
       this.state = {};
+      this.submitForm = this.submitForm.bind(this);
+      this.submitProductForm = this.submitProductForm.bind(this);
+    }
+
+    componentDidMount () {
+        window.addEventListener('submit', this.submitForm);
+    }
+
+    submitForm (e) {
+        let isProductForm = hasClass(e.target, 'js-product-form');
+        let serialized = serialize(e.target, { hash: true });
+
+        e.preventDefault();
+
+        if (isProductForm) {
+            this.submitProductForm(serialized);
+        }
+    }
+
+    submitProductForm (data) {
+        console.log(data);
     }
 
     render () {
-
         return (<div></div>);
     }
 }
