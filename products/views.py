0c891e0cc3768da69d591e8e06310dd49487efba
ntuckovic <nikola.tuckovic@styria.hr>
2016-11-12 15:23:02 +0100

    building list components

--- products/views.py
+++ products/views.py
@@ -16,3 +16,10 @@ class ProductListView(ListView):
         qs = qs.filter(active=True)
 
         return qs
+
+    def get_context_data(self, **kwargs):
+        context_data = super(ProductListView, self).get_context_data(**kwargs)
+
+        context_data['amount_range'] = range(1, 101)
+
+        return context_data
