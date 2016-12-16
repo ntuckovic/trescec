03e94aa6af61021f062b6bd30c3865d4ecefd337
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-16 21:45:07 +0100

    flatblock i daj kontakt

--- flatpages_override/admin.py
+++ flatpages_override/admin.py
@@ -7,6 +7,8 @@ from django.contrib import admin
 from django.contrib.flatpages.admin import FlatPageAdmin
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.forms import FlatpageForm
+from flatblocks.models import FlatBlock
+
 from django.utils.translation import ugettext_lazy as _
 
 from ckeditor_uploader.widgets import CKEditorUploadingWidget
@@ -37,3 +39,23 @@ class FlatPageAdmin(FlatPageAdmin):
 # Re-register FlatPageAdmin
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+
+
+class FlatBlockForm(forms.ModelForm):
+    content = forms.CharField(
+        widget=CKEditorUploadingWidget(config_name='awesome_ckeditor')
+    )
+
+    class Meta:
+        model = FlatBlock
+        fields = ('slug', 'header', 'content')
+
+
+class FlatBlockAdmin(admin.ModelAdmin):
+    form = FlatBlockForm
+    ordering = ['slug', ]
+    list_display = ('slug', 'header')
+    search_fields = ('slug', 'header', 'content')
+
+admin.site.unregister(FlatBlock)
+admin.site.register(FlatBlock, FlatBlockAdmin)
