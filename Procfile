a3e0001e7f112643ebeb0d61d54c2dd9bc7abe3c
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-24 22:55:42 +0100

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn trescec.wsgi --log-file -
---
+++ trescec/settings/production.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import os
+from base import *
+
+DEBUG = False
+
+DEBUG_TOOLBAR = False
+CACHE = True
+CACHALOT = True
