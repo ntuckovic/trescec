c8e41ad9a8d096cbf972a5d65a0bfeee08b0ee9b
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-25 09:54:06 +0100

    heroku support

--- trescec/settings/production.py
+++ trescec/settings/production.py
@@ -2,9 +2,19 @@
 
 import os
 from base import *
+import dj_database_url
 
 DEBUG = False
 
 DEBUG_TOOLBAR = False
 CACHE = True
 CACHALOT = True
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
