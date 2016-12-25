09d3c7e23cf9dc8aa73ca284a90c21950569cf5e
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-25 10:33:34 +0100

    opet sranja

--- trescec/settings/production.py
+++ trescec/settings/production.py
@@ -10,7 +10,11 @@ DEBUG_TOOLBAR = False
 CACHE = True
 CACHALOT = True
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static/'),
+)
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
