9a6c0113e947a5770efb43b94c40456b7ec4255d
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-25 10:34:55 +0100

    jel

--- trescec/settings/production.py
+++ trescec/settings/production.py
@@ -11,10 +11,6 @@ CACHE = True
 CACHALOT = True
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static/'),
-)
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
@@ -29,12 +25,12 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles/')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/'),
 )
 
 # Simplified static file serving.
