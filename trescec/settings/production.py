6fb1dd3f728e9ab34e36e25a4639b31842a746b8
ntuckovic <nikola.tuckovic@styria.hr>
2016-12-25 10:14:03 +0100

    još jednom

--- trescec/settings/production.py
+++ trescec/settings/production.py
@@ -10,11 +10,29 @@ DEBUG_TOOLBAR = False
 CACHE = True
 CACHALOT = True
 
-db_from_env = dj_database_url.config(conn_max_age=500)
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
